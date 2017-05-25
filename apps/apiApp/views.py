925169eeb9117efa5024b1b06facdb2653afbed5
hukekuan <hukekuan@163.com>
2017-05-25 21:20:37 +0800

    初始化更新

--- apps/apiApp/views.py
+++ apps/apiApp/views.py
@@ -1,8 +1,17 @@
 #-*- coding:utf-8 -*-
 #!/usr/bin/env python
 
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
+
+
 
 class HelloWorld(Resource):
     def get(self,name):
         return {'hello': 'world '+ name}
+
+    def post(self,name):
+        parser = reqparse.RequestParser()
+        parser.add_argument('task', type=str)
+        args = parser.parse_args()
+        print(args)
+        return {'status': 'success'}
