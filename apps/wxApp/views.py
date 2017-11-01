de0b5bc329715b3330b1f394be7c4174074855f1
Administrator <hukekuan@163.com>
2017-11-01 22:25:52 +0800

    初始化

--- apps/wxApp/views.py
+++ apps/wxApp/views.py
@@ -4,8 +4,8 @@ import json
 import sys
 import time
 import urllib
-
 from flask import render_template
+from apps.wxApp.utils import parse_xml
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -13,11 +13,40 @@ from flask import request
 
 from apps.wxApp import wx
 
-@wx.route('/',methods=['GET','POST'])
-def Token():
-    result = request.args['echostr']
-    return result
+# @wx.route('/token',methods=['GET','POST'])
+# def Token():
+#     result = request.args['echostr']
+#     return result
 
-@wx.route('/test')
-def Test():
-    return render_template('wx/wxindex.html', **locals())
+
+@wx.route('/token',methods=['GET','POST'])
+def Token():
+    if True:
+        receiveMsg = parse_xml(request.data)
+        XmlForm = """
+                <xml>
+                <ToUserName><![CDATA[{ToUserName}]]></ToUserName>
+                <FromUserName><![CDATA[{FromUserName}]]></FromUserName>
+                <CreateTime>{CreateTime}</CreateTime>
+                <MsgType><![CDATA[news]]></MsgType>
+                <ArticleCount>2</ArticleCount>
+                <Articles>
+                <item>
+                <Title><![CDATA[中乌发动机合作生变？乌克兰地方法院冻结中资股份]]></Title>
+                <PicUrl><![CDATA[http://c3gis-c3gis.stor.sinaapp.com/images/a.jpeg]]></PicUrl>
+                <Url><![CDATA[http://c3gis.applinzi.com]]></Url>
+                </item>
+                <item>
+                <Title><![CDATA[中国新型无人武直惊艳亮相，优势独特全世界仅3国能造]]></Title>
+                <PicUrl><![CDATA[http://c3gis-c3gis.stor.sinaapp.com/images/a.jpeg]]></PicUrl>
+                <Url><![CDATA[http://c3gis.applinzi.com]]></Url>
+                </item>
+                </Articles>
+                </xml>
+            """
+        info = dict()
+        info['ToUserName'] = receiveMsg.FromUserName
+        info['FromUserName'] = receiveMsg.ToUserName
+        info['CreateTime'] = int(time.time())
+
+        return XmlForm.format(**info)
