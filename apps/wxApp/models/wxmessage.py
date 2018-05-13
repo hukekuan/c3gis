b9afe065628c155238223ac4d2f55cfc72bb2683
Administrator <hukekuan@163.com>
2018-05-13 22:23:20 +0800

    初始化

--- apps/wxApp/models/wxmessage.py
+++ apps/wxApp/models/wxmessage.py
@@ -3,6 +3,111 @@
 import string
 import time
 
+##################################################文本消息#######################################################
+class TextMsg(object):
+    def __init__(self, fromUserName, toUserName,text):
+        self.fromUserName = fromUserName
+        self.toUserName = toUserName
+        self.text = text
+
+    def xmlFormat(self):
+        resultStr = '''
+        <xml>
+            <ToUserName>< ![CDATA[{toUser}] ]></ToUserName>
+            <FromUserName>< ![CDATA[{fromUser}] ]></FromUserName>
+            <CreateTime>{createTime}</CreateTime>
+            <MsgType>< ![CDATA[text] ]></MsgType>
+            <Content>< ![CDATA[{text}] ]></Content>
+        </xml>
+        '''
+
+##################################################图片消息#######################################################
+class imageMsg(object):
+    def __init__(self, fromUserName, toUserName,text):
+        self.fromUserName = fromUserName
+        self.toUserName = toUserName
+        self.text = text
+
+    def xmlFormat(self):
+        resultStr = '''
+        <xml>
+        <ToUserName>< ![CDATA[{toUser}] ]></ToUserName>
+        <FromUserName>< ![CDATA[{fromUser}] ]></FromUserName>
+        <CreateTime>{createTime}</CreateTime>
+        <MsgType>< ![CDATA[image] ]></MsgType>
+        <Image>
+           <MediaId>< ![CDATA[{mediaId}] ]></MediaId>
+        </Image>
+        </xml>
+        '''
+
+##################################################语音消息#######################################################
+class voiceMsg(object):
+    def __init__(self, fromUserName, toUserName,text):
+        self.fromUserName = fromUserName
+        self.toUserName = toUserName
+        self.text = text
+
+    def xmlFormat(self):
+        resultStr = '''
+        <xml>
+        <ToUserName>< ![CDATA[{toUser}] ]></ToUserName>
+        <FromUserName>< ![CDATA[{fromUser}] ]></FromUserName>
+        <CreateTime>{createTime}</CreateTime>
+        <MsgType>< ![CDATA[voice] ]></MsgType>
+        <Voice>
+           <MediaId>< ![CDATA[{mediaId}] ]></MediaId>
+        </Voice>
+        </xml>
+        '''
+
+##################################################视频消息#######################################################
+class videoMsg(object):
+    def __init__(self, fromUserName, toUserName,text):
+        self.fromUserName = fromUserName
+        self.toUserName = toUserName
+        self.text = text
+
+    def xmlFormat(self):
+        resultStr = '''
+        <xml>
+        <ToUserName>< ![CDATA[{toUser}] ]></ToUserName>
+        <FromUserName>< ![CDATA[{fromUser}] ]></FromUserName>
+        <CreateTime>{createTime}</CreateTime>
+        <MsgType>< ![CDATA[video] ]></MsgType>
+        <Video>
+            <MediaId>< ![{mediaId}] ]></MediaId>
+            <Title>< ![CDATA[{title}] ]></Title>
+            <Description>< ![CDATA[{description}] ]></Description>
+        </Video>
+        </xml>
+        '''
+
+##################################################音乐消息#######################################################
+class musicMsg(object):
+    def __init__(self, fromUserName, toUserName,text):
+        self.fromUserName = fromUserName
+        self.toUserName = toUserName
+        self.text = text
+
+    def xmlFormat(self):
+        resultStr = '''
+        <xml>
+        <ToUserName>< ![CDATA[toUser] ]></ToUserName>
+        <FromUserName>< ![CDATA[fromUser] ]></FromUserName>
+        <CreateTime>12345678</CreateTime>
+        <MsgType>< ![CDATA[music] ]></MsgType>
+        <Music>
+            <Title>< ![CDATA[TITLE] ]></Title>
+            <Description>< ![CDATA[DESCRIPTION] ]></Description>
+            <MusicUrl>< ![CDATA[MUSIC_Url] ]></MusicUrl>
+            <HQMusicUrl>< ![CDATA[HQ_MUSIC_Url] ]></HQMusicUrl>
+            <ThumbMediaId>< ![CDATA[media_id] ]></ThumbMediaId>
+        </Music>
+        </xml>
+        '''
+
+##################################################图文消息#######################################################
 class ArticleItem(object):
     def __init__(self,title,picUrl,url):
         self.title=title
