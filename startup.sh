8530de6b409559c3d58d723ba5ac809f1ae59dec
Administrator <hukekuan@163.com>
2018-05-13 10:11:44 +0800

    初始化

---
+++ startup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source ../../envs/c3gis/bin/activate
+
+portnum=`lsof -i:5000| awk 'NR > 1' | wc -l`
+
+if [ $portnum -gt 0 ]
+then
+kill -s 9 `sudo lsof -i:5000| awk 'NR > 1 {print $2}'`
+fi
+
+gunicorn -k gevent -c ./gun.conf app:app
